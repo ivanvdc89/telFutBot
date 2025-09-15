49b883a16c166ff0ba873513b41bdee801ded4b1
Ivan <ivanvdc89@gmail.com>
2025-09-15 00:14:34 +0000

    basic response done

--- basic.php
+++ basic.php
@@ -8,7 +8,7 @@ $telegram = new BotApi('8363817321:AAGIQ7mQ_hTZgXduSiuYKdAEAQyeMS-bAHY');
 
 $update = json_decode(file_get_contents('php://input'));
 
-if(isset($update->message)) {
+if(isset($update->message->text)) {
     $chatId = $update->message->chat->id;
     $text = $update->message->text;
 
