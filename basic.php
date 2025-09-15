c49d36a15e5ded1cb40f56b5478819e480dd5deb
Ivan <ivanvdc89@gmail.com>
2025-09-15 00:07:51 +0000

    basic response done

--- basic.php
+++ basic.php
@@ -1,3 +1,20 @@
 <?php
 
+include './vendor/autoload.php';
+
+use TelegramBot\Api\BotApi;
+
+$telegram = new BotApi('8363817321:AAGIQ7mQ_hTZgXduSiuYKdAEAQyeMS-bAHY');
+
+$update = json_decode(file_get_contents('php://input'));
+
+if(isset($update->message)) {
+    $chatId = $update->message->chat->id;
+    $text = $update->message->text;
+
+    // Echo the received message back to the user
+    $telegram->sendMessage($chatId, "You said: " . $text);
+}
+
 ?>
+
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '81c7d9364d22bb97cc51601022d25516b2f1160f',
+        'reference' => '42213e616d8e5279cf7504af91836515f8499c79',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '81c7d9364d22bb97cc51601022d25516b2f1160f',
+            'reference' => '42213e616d8e5279cf7504af91836515f8499c79',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
