2c227375859b1f6c40c1315c20d43b3392770a37
Ivan Barrachina <ivan.barrachina@promofarma.com>
2025-09-15 03:29:48 +0200

    add files

--- basic.php
+++ basic.php
@@ -12,6 +12,21 @@ if(isset($update->message->text)) {
     $chatId = $update->message->chat->id;
     $text = $update->message->text;
 
+    if($text === '/rules') {
+        $replyMarkup = $telegram->sendPhoto($chatId, "https://example.com/rules.jpg", "Regles del joc", null, true);
+        exit;
+    }
+
+    if($text === '/teams') {
+        $keyboard = [
+            ['Option 1', 'Option 2'],
+            ['Option 3', 'Option 4']
+        ];
+        $replyMarkup = $telegram->sendPhoto($keyboard, true, true);
+        $telegram->sendMessage($chatId, "Benvingut! Tria una opció:", false, null, null, $replyMarkup);
+        exit;
+    }
+
     $telegram->sendMessage($chatId, "Per ara millor utilitzar els botons -> /start");
 }
 
