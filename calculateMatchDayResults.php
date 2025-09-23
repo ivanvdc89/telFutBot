00ea40f5d2660a5482306fe3a0e99dc67428960f
Ivan Barrachina <ivan.barrachina@promofarma.com>
2025-09-24 01:35:49 +0200

    calculateMatchDayResults command added

--- calculateMatchDayResults.php
+++ calculateMatchDayResults.php
@@ -6,7 +6,30 @@ require_once("config/connection.php");
 require_once("models/player.php");
 require_once("models/teamPoints.php");
 
-$playersRepo = new Player();
+$playersRepo    = new Player();
+$teamsRepo      = new Team();
+$teamResultRepo = new teamResult();
+$teamPointRepo  = new TeamPoint();
 
 $players = $playersRepo->getAllPlayers();
 
+foreach ($players as $player) {
+    $playerId = $player['id'];
+    $playerTeams = $teamsRepo->getTeamsByPlayerId($player['id']);
+    foreach ($playerTeams as $team) {
+        $pot        = $team['pot'];
+        $teamPoints = $teamPointRepo->getPointByTeamIdAndMatchday($team['id'], 1);
+        if (count($teamPoints) == 0) {
+            continue;
+        }
+        $teamResultRepo->addTeamPoints(
+            $playerId,
+            1,
+            $pot,
+            $team['id'],
+            $teamPoints[0]['points'],
+            '',
+            $teamPoints[0]['points']
+        );
+    }
+}
