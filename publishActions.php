0fbd04387b2a52d550b8d4bc494cc1e5bee73177
Ivan Barrachina <ivan.barrachina@promofarma.com>
2025-12-09 18:47:42 +0100

    improve publishActions with kosAndShields info

--- publishActions.php
+++ publishActions.php
@@ -158,7 +158,7 @@ if ($doubleOrNothingActive) {
 if ($kosAndShieldsActive) {
     arsort($sumKos);
     $numKos = 4;
-    $message .= $kosMessage . "\n Amb més vots de KO:\n";
+    $message .= "\n" . $kosMessage . "Amb més vots de KO:\n";
     foreach ($sumKos as $teamId => $votes) {
         $team = $teamsRepo->getTeamById($teamId);
         $message .= "-Equip KO: " . $team[0]['name'] . "\n";
