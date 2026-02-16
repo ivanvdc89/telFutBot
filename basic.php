9ebbb496e6205b28d9db1e2ad05f3f8ec9eb1a79
Ivan Barrachina <ivan.barrachina@promofarma.com>
2026-02-16 18:55:35 +0100

    conf to j10, first match of playIn

--- basic.php
+++ basic.php
@@ -456,12 +456,24 @@ Exemples, si t'actives el #guanyarOMorir en Champions:
         $playerTeams = $teamsRepo->getTeamsByPlayerId($player[0]['id']);
         $rows        = [];
         $row         = [];
+        $numPot      = 1;
         foreach ($playerTeams as $team) {
-            $row[] = '/out ' . $team['name'];
+            if ($team['pot'] != $numPot) {
+                if ($numPot < 5) {
+                    $row[] = '/out CHL_Pot_' . $numPot%4 == 0 ? 4 : $numPot%4;
+                } elseif ($numPot < 9) {
+                    $row[] = '/out EUL_Pot_' . $numPot%4 == 0 ? 4 : $numPot%4;
+                } else {
+                    $row[] = '/out COL_Pot_' . $numPot%4 == 0 ? 4 : $numPot%4;
+                }
+            } else {
+                $row[] = '/out ' . $team['name'];
+            }
             if (count($row) == 3) {
                 $rows[] = $row;
                 $row    = [];
             }
+            $numPot++;
         }
 
         if (count($row) != 0) {
