116a521561cc961c399c3e7babadee08357ed9f4
Ivan Barrachina <ivan.barrachina@promofarma.com>
2026-02-24 12:46:25 +0100

    j10 calculatePoints

--- publishResults.php
+++ publishResults.php
@@ -21,7 +21,7 @@ $matchDayTeamPointsRepo   = new MatchDayTeamPoint();
 
 $group          = $groupRepo->getGroup(1);
 $groupChatId    = $group[0]['chat_id'];
-$matchDay       = 9;
+$matchDay       = 10;
 $message        = "";
 $order          = 1;
 $messageBestsCHL = "";
